c2c0296044a67adbae4dd377c8f2fc110fe641b5
Sultan <531213@student.fontys.nl>
2024-10-31 16:51:30 +0100

    test

--- ims-frontend/jest.config.js
+++ ims-frontend/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
