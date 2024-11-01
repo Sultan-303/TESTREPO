e276a23ce9849f0bdf6058e8eafda86c4c5e2c85
Sultan <531213@student.fontys.nl>
2024-11-01 10:34:29 +0100

    tet

--- ims-frontend/cypress/integration/items.spec.js
+++ ims-frontend/cypress/integration/items.spec.js
@@ -1,30 +1,30 @@
 describe('Items Page', () => {
-  it('should load the items page', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/items');
+  });
+
+  it('should load the items page', () => {
     cy.contains('Items');
   });
 
   it('should add a new item', () => {
-    cy.visit('http://localhost:3000/items');
     cy.contains('Add Item').click();
-    cy.get('input[name="itemName"]').type('New Item');
-    cy.get('input[name="unit"]').type('pcs');
-    cy.get('input[name="price"]').type('5.00');
+    cy.get('input[name="itemName"]', { timeout: 10000 }).should('be.visible').type('New Item');
+    cy.get('input[name="unit"]', { timeout: 10000 }).should('be.visible').type('pcs');
+    cy.get('input[name="price"]', { timeout: 10000 }).should('be.visible').type('5.00');
     cy.contains('Add Item').click();
     cy.contains('New Item');
   });
 
   it('should edit an item', () => {
-    cy.visit('http://localhost:3000/items');
-    cy.contains('Edit').click();
-    cy.get('input[name="itemName"]').clear().type('Updated Item');
+    cy.contains('Edit', { timeout: 10000 }).should('be.visible').click();
+    cy.get('input[name="itemName"]', { timeout: 10000 }).clear().type('Updated Item');
     cy.contains('Save').click();
     cy.contains('Updated Item');
   });
 
   it('should delete an item', () => {
-    cy.visit('http://localhost:3000/items');
-    cy.contains('Delete').click();
+    cy.contains('Delete', { timeout: 10000 }).should('be.visible').click();
     cy.contains('There are no items to be viewed.');
   });
 });
