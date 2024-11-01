981485c3df332f4035e861232f38eba377212e51
Sultan <531213@student.fontys.nl>
2024-11-01 09:39:21 +0100

    test

--- IMSTests/obj/Debug/net8.0/IMSTests.AssemblyInfo.cs
+++ IMSTests/obj/Debug/net8.0/IMSTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMSTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51637b38c3b6bb29dc99d639ba9bf51a8209962e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726fcec8ae861844f68f31f9b5f1db7aa942f303")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMSTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMSTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
